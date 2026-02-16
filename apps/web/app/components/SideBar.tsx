2c9d403fc89b9bf6ece3910c1599485e800827f0
a-y-a-n-das <ayandas8125@gmail.com>
2026-02-16 23:32:41 +0530

    fix

--- apps/web/app/components/SideBar.tsx
+++ apps/web/app/components/SideBar.tsx
@@ -18,7 +18,7 @@ export default function SideBar() {
       router.push("/dashboard");
     } else if (button === "workoutStats") {
       setDashboardOpen(false);
-      setWorkoutStatsO$2b$12$hqxpsq3y0JzTIjB1CWttAeFHKR8seSbMrRE.v2PpQCzVKt3U8aTh6pen(true);
+      setWorkoutStatsOpen(true);
       setScheduleOpen(false);
       router.push("/dashboard/workouts");
     } else if (button === "schedule") {
